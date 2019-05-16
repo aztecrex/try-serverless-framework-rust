e12988b76d5a23a7af3507e972e0f2d839e8e207
Greg Wiley <gwiley@cj.com>
2019-05-16 14:02:53 -0700

    checkpoint

--- handler.sh
+++
@@ -1,8 +0,0 @@
-function hello () {
-  EVENT_DATA=$1
-  echo "$EVENT_DATA" 1>&2;
-  RESPONSE='{"isBase64Encoded": false, "headers": {}, "statusCode":200, "body":"{\"msg\": \"sup\"}"}'
-
-  echo "$RESPONSE"
-}
-
